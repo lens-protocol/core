b9a5f42942918a5d19fecc6a522039c557411405
donosonaumczuk <adonoso@itba.edu.ar>
2022-03-28 19:17:04 +0100

    Small re-arrange to save more gas

--- contracts/core/LensHub.sol
+++ contracts/core/LensHub.sol
@@ -532,8 +532,8 @@ contract LensHub is ILensHub, LensNFTBase, VersionedInitializable, LensMultiStat
         override
         whenNotPaused
     {
-        bytes32[] memory dataHashes = new bytes32[](vars.datas.length);
         uint256 dataLength = vars.datas.length;
+        bytes32[] memory dataHashes = new bytes32[](dataLength);
         for (uint256 i = 0; i < dataLength; ) {
             dataHashes[i] = keccak256(vars.datas[i]);
             unchecked {
