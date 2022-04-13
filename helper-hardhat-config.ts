11ac489b7c00224bbf4e9de65438344f08ae2b64
donosonaumczuk <adonoso@itba.edu.ar>
2022-04-13 17:03:01 +0100

    feat: Environment variables modified to be generic

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -8,30 +8,18 @@ import {
 import dotenv from 'dotenv';
 dotenv.config({});
 
-const INFURA_KEY = process.env.INFURA_KEY || '';
-const ALCHEMY_KEY = process.env.ALCHEMY_KEY || '';
 const TENDERLY_FORK_ID = process.env.TENDERLY_FORK_ID || '';
 
 const GWEI = 1000 * 1000 * 1000;
 
 export const NETWORKS_RPC_URL: iParamsPerNetwork<string> = {
-  [eEthereumNetwork.kovan]: ALCHEMY_KEY
-    ? `https://eth-kovan.alchemyapi.io/v2/${ALCHEMY_KEY}`
-    : `https://kovan.infura.io/v3/${INFURA_KEY}`,
-  [eEthereumNetwork.ropsten]: ALCHEMY_KEY
-    ? `https://eth-ropsten.alchemyapi.io/v2/${ALCHEMY_KEY}`
-    : `https://ropsten.infura.io/v3/${INFURA_KEY}`,
-  [eEthereumNetwork.main]: ALCHEMY_KEY
-    ? `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`
-    : `https://mainnet.infura.io/v3/${INFURA_KEY}`,
+  [eEthereumNetwork.kovan]: process.env.KOVAN_RPC_URL,
+  [eEthereumNetwork.ropsten]: process.env.ROPSTEN_RPC_URL,
+  [eEthereumNetwork.main]: process.env.MAINNET_RPC_URL,
   [eEthereumNetwork.hardhat]: 'http://localhost:8545',
   [eEthereumNetwork.harhatevm]: 'http://localhost:8545',
   [eEthereumNetwork.tenderlyMain]: `https://rpc.tenderly.co/fork/${TENDERLY_FORK_ID}`,
-  [ePolygonNetwork.mumbai]: ALCHEMY_KEY
-  ? `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_KEY}`
-  : `https://polygon-mumbai.infura.io/v3/${INFURA_KEY}`,
-  [ePolygonNetwork.matic]: ALCHEMY_KEY
-  ? `https://polygon-mainnet.g.alchemy.com/v2/${ALCHEMY_KEY}`
-  : `https://polygon-mainnet.infura.io/v3/${INFURA_KEY}`,
+  [ePolygonNetwork.mumbai]: process.env.MUMBAI_RPC_URL,
+  [ePolygonNetwork.matic]: process.env.POLYGON_RPC_URL,
   [eXDaiNetwork.xdai]: 'https://rpc.xdaichain.com/',
 };
