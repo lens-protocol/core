a5391af4c98f9dc6c418584e8ce0891007479f44
danilo neves cruz <cruzdanilo@gmail.com>
2022-05-26 14:29:12 -0300

    misc: use solidity version caret range for interfaces and libraries

--- contracts/libraries/Constants.sol
+++ contracts/libraries/Constants.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.10;
+pragma solidity ^0.8.10;
 
 library Constants {
     string internal constant FOLLOW_NFT_NAME_SUFFIX = '-Follower';
--- contracts/libraries/Helpers.sol
+++ contracts/libraries/Helpers.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.10;
+pragma solidity ^0.8.10;
 
 import {DataTypes} from './DataTypes.sol';
 import {Errors} from './Errors.sol';
