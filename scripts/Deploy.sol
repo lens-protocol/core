a9e36edaf259469aa0796516c136964b7ef64a83
Devan Non <devanon@chainstride.capital>
2022-06-10 20:04:22 +0200

    Deploy script progress

--- scripts/Deploy.sol
+++ scripts/Deploy.sol
@@ -1,17 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import {Helper} from '../contracts/mocks/Helper.sol';
 import {ModuleGlobals} from '../contracts/core/modules/ModuleGlobals.sol';
 
-import {Script} from 'forge-std/Script.sol';
+import 'forge-std/Script.sol';
 
 contract Deploy is Script {
+    uint16 constant TREASURY_FEE_BPS = 50;
+
+    // TODO: Replace with loading in addresses from mnemonic?
+    address immutable user = vm.addr(1);
+    address immutable userTwo = vm.addr(2);
+    address immutable userThree = vm.addr(3);
+    address immutable governance = vm.addr(4);
+    address immutable treasury = vm.addr(5);
+
     function run() external {
         vm.startBroadcast();
 
-        Helper helper = new Helper();
-        ModuleGlobals moduleGlobals = new ModuleGlobals();
+        ModuleGlobals moduleGlobals = new ModuleGlobals(governance, treasury, TREASURY_FEE_BPS);
+
+        uint256 deployerNonce = vm.getNonce(msg.sender);
+
+        uint256 followNFTNonce = deployerNonce + 1;
+        uint256 collectNFTNonce = deployerNonce + 2;
+        uint256 hubProxyNonce = deployerNonce + 3;
+
+    //      address followNFTImplAddress = address(
+    // keccak256(RLP.encode([deployer.address, followNFTNonce])).substr(26)
+    // );
 
         vm.stopBroadcast();
     }
