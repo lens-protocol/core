6369189130c218176859fd8376fed29f29855cd4
donosonaumczuk <adonoso@itba.edu.ar>
2022-08-20 16:36:11 -0500

    feat: CollectNFT now implements EIP-2981 royalty standard

--- contracts/core/CollectNFT.sol
+++ contracts/core/CollectNFT.sol
@@ -3,10 +3,12 @@
 pragma solidity 0.8.10;
 
 import {ICollectNFT} from '../interfaces/ICollectNFT.sol';
+import {IERC721} from '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 import {ILensHub} from '../interfaces/ILensHub.sol';
 import {Errors} from '../libraries/Errors.sol';
 import {Events} from '../libraries/Events.sol';
 import {LensNFTBase} from './base/LensNFTBase.sol';
+import {ERC721Enumerable} from './base/ERC721Enumerable.sol';
 
 /**
  * @title CollectNFT
@@ -24,11 +26,18 @@ contract CollectNFT is LensNFTBase, ICollectNFT {
 
     bool private _initialized;
 
+    uint256 internal _royaltyBasisPoints;
+
+    // bytes4(keccak256('royaltyInfo(uint256,uint256)')) == 0x2a55205a
+    bytes4 internal constant INTERFACE_ID_ERC2981 = 0x2a55205a;
+    uint16 internal constant BASIS_POINTS = 10000;
+
     // We create the CollectNFT with the pre-computed HUB address before deploying the hub proxy in order
     // to initialize the hub proxy at construction.
     constructor(address hub) {
         if (hub == address(0)) revert Errors.InitParamsInvalid();
         HUB = hub;
+        _royaltyBasisPoints = 1000; // 10% of royalties
         _initialized = true;
     }
 
@@ -65,6 +74,58 @@ contract CollectNFT is LensNFTBase, ICollectNFT {
     function tokenURI(uint256 tokenId) public view override returns (string memory) {
         if (!_exists(tokenId)) revert Errors.TokenDoesNotExist();
         return ILensHub(HUB).getContentURI(_profileId, _pubId);
+    }
+
+    /**
+     * @notice Changes the royalty percentage for secondary sales. Can only be called publication's
+     * profile owner.
+     * 
+     * @param royaltyBasisPoints The royalty percentage meassured in basis points. Each basis point
+     * represents 0.01%.
+     */
+    function setRoyalty(uint256 royaltyBasisPoints) external {
+        if (IERC721(HUB).ownerOf(_profileId) == msg.sender) {
+            if (royaltyBasisPoints > BASIS_POINTS) {
+                revert Errors.InvalidParameter();
+            } else {
+                _royaltyBasisPoints = royaltyBasisPoints;
+            }
+        } else {
+            revert Errors.NotProfileOwner();
+        }
+    }
+
+    /**
+     * @notice Called with the sale price to determine how much royalty 
+     * is owed and to whom.
+     * 
+     * @param tokenId The token ID of the NFT queried for royalty information.
+     * @param salePrice The sale price of the NFT specified.
+     * @return A tuple with the address who should receive the royalties and the royalty
+     * payment amount for the given sale price.
+     */ 
+    function royaltyInfo(uint256 tokenId, uint256 salePrice)
+        external
+        view
+        returns (address, uint256)
+    {
+        return (
+            IERC721(HUB).ownerOf(_profileId),
+            (salePrice * _royaltyBasisPoints) / BASIS_POINTS
+        );
+    }    
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ERC721Enumerable)
+        returns (bool)
+    {
+        return interfaceId == INTERFACE_ID_ERC2981 || super.supportsInterface(interfaceId);
     }
 
     /**
