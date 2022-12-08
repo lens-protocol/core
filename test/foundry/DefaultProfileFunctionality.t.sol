f5fe687decc933002906c70a053f118c57d935a0
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2022-12-08 16:25:06 +0200

    feat: Added more scen tests

--- test/foundry/DefaultProfileFunctionality.t.sol
+++ test/foundry/DefaultProfileFunctionality.t.sol
@@ -15,4 +15,35 @@ contract DefaultProfileFunctionalityTest is BaseTest {
         vm.expectRevert(Errors.NotProfileOwner.selector);
         hub.setDefaultProfile(otherSigner, FIRST_PROFILE_ID);
     }
+
+    // SCENARIOS
+
+    function testCanSetDefaultProfile() public {
+        vm.prank(profileOwner);
+        hub.setDefaultProfile(profileOwner, FIRST_PROFILE_ID);
+        assertEq(hub.getDefaultProfile(profileOwner), FIRST_PROFILE_ID);
+    }
+
+    function testCanSetThenUnsetDefaultProfile() public {
+        vm.startPrank(profileOwner);
+        hub.setDefaultProfile(profileOwner, FIRST_PROFILE_ID);
+        assertEq(hub.getDefaultProfile(profileOwner), FIRST_PROFILE_ID);
+        hub.setDefaultProfile(profileOwner, 0);
+        assertEq(hub.getDefaultProfile(profileOwner), 0);
+
+        vm.stopPrank();
+    }
+
+    function testCanSetThenChangeDefaultProfile() public {
+        vm.prank(profileOwner);
+        hub.setDefaultProfile(profileOwner, FIRST_PROFILE_ID);
+        assertEq(hub.getDefaultProfile(profileOwner), FIRST_PROFILE_ID);
+
+        vm.prank(me);
+        uint256 newProfileId = hub.createProfile(mockCreateProfileData);
+
+        vm.prank(profileOwner);
+        hub.setDefaultProfile(profileOwner, newProfileId);
+        assertEq(hub.getDefaultProfile(profileOwner), newProfileId);
+    }
 }
