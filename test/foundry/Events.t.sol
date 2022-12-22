165c95cb3154d0e82beb710e5cc23882c4d0e1eb
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2022-12-22 17:17:29 +0200

    feat: Create base Event test file

---
+++ test/foundry/Events.t.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import './base/BaseTest.t.sol';
+
+contract EventTest is BaseTest {
+    function setUp() public override {
+        TestSetup.setUp();
+    }
+
+    // MISC
+
+    // HUB GOVERNANCE
+
+    // HUB INTERACTION
+
+    // MODULE GLOBALS GOVERNANCE
+}
