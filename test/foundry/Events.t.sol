262d17fa78d8b8d969a22651f00c27a9c0d949fe
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2022-12-23 21:26:22 +0200

    feat: Scaffold events tests in foundry

--- test/foundry/Events.t.sol
+++ test/foundry/Events.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import './base/BaseTest.t.sol';
+import "./base/BaseTest.t.sol";
 
 contract EventTest is BaseTest {
     function setUp() public override {
@@ -10,9 +10,51 @@ contract EventTest is BaseTest {
 
     // MISC
 
+    function testProxyInitEmitsExpectedEvents() public {}
+
     // HUB GOVERNANCE
+
+    function testGovernanceEmitsExpectedEvents() public {}
+
+    function testEmergencyAdminChangeEmitsExpectedEvents() public {}
+
+    function testProtocolStateChangeEmitsExpectedEvents() public {}
+
+    function testFollowModuleWhitelistEmitsExpectedEvents() public {}
+
+    function testReferenceModuleWhitelistEmitsExpectedEvents() public {}
+
+    function testCollectModuleWhitelistEmitsExpectedEvents() public {}
 
     // HUB INTERACTION
 
+    function testProfileCreationEmitsExpectedEvents() public {}
+
+    function testProfileCreationForOtherUserEmitsExpectedEvents() public {}
+
+    function testSettingFollowModuleEmitsExpectedEvents() public {}
+
+    function testSettingDispatcherEmitsExpectedEvents() public {}
+
+    function testPostingEmitsExpectedEvents() public {}
+
+    function testCommentingEmitsExpectedEvents() public {}
+
+    function testMirroringEmitsExpectedEvents() public {}
+
+    function testFollowingEmitsExpectedEvents() public {}
+
+    function testCollectingEmitsExpectedEvents() public {}
+
+    function testCollectingFromMirrorEmitsExpectedEvents() public {}
+
     // MODULE GLOBALS GOVERNANCE
+
+    function testGovernanceChangeEmitsExpectedEvents() public {}
+
+    function testTreasuryChangeEmitsExpectedEvents() public {}
+
+    function testTreasuryFeeChangeEmitsExpectedEvents() public {}
+
+    function testCurrencyWhitelistEmitsExpectedEvents() public {}
 }
