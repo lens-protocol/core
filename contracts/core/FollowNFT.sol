ea220eac37180f60e091756a37af7b00eecb8551
donosonaumczuk <adonoso@itba.edu.ar>
2023-01-30 16:22:09 -0300

    misc: Comment refactor for consistency

--- contracts/core/FollowNFT.sol
+++ contracts/core/FollowNFT.sol
@@ -105,21 +105,23 @@ contract FollowNFT is HubRestricted, LensNFTBase, ERC2981CollectionRoyalties, IF
         }
         address followTokenOwner = _unsafeOwnerOf(followTokenId);
         if (followTokenOwner == address(0)) {
-            // Follow token is: Unwrapped
-            // Unfollowing and allowing recovery
+            // Follow token is unwrapped.
+            // Unfollowing and allowing recovery.
             _unfollow({unfollower: unfollowerProfileId, followTokenId: followTokenId});
             _followDataByFollowTokenId[followTokenId]
                 .profileIdAllowedToRecover = unfollowerProfileId;
         } else {
-            // Follow token is: Wrapped
+            // Follow token is wrapped.
             address unfollowerProfileOwner = IERC721(HUB).ownerOf(unfollowerProfileId);
-            // Follower Profile or DE should hold a token or be ApprovedForAll
+            // Follower profile owner or its approved delegated executor must hold the token or be approved-for-all.
             if (
                 (followTokenOwner != unfollowerProfileOwner) &&
                 (followTokenOwner != executor) &&
                 !isApprovedForAll(followTokenOwner, executor) &&
                 !isApprovedForAll(followTokenOwner, unfollowerProfileOwner)
-            ) revert DoesNotHavePermissions();
+            ) {
+                revert DoesNotHavePermissions();
+            }
             _unfollow({unfollower: unfollowerProfileId, followTokenId: followTokenId});
         }
     }
