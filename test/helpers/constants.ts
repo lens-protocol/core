76895a51bafa06df6e0308789cde8026441f1a71
donosonaumczuk <adonoso@itba.edu.ar>
2023-04-21 19:13:55 +0100

    misc: Legacy tests removed

--- test/helpers/constants.ts
+++
@@ -1,8 +0,0 @@
-export const MAX_UINT256 = '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff';
-export const WETH_ADDRESS = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
-export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
-
-// Fetched from $npx hardhat node, account # 7
-export const FAKE_PRIVATEKEY = '0xa2e0097c961c67ec197b6865d7ecea6caffc68ebeb00e6050368c8f67fc9c588';
-
-export const HARDHAT_CHAINID = 31337;
