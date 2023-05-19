537215b238c485d0cfaeb1719f2654558f0be521
vicnaum <vicnaum@gmail.com>
2023-05-19 21:18:27 +0200

    misc: improved executeAsGovernance revert bubbling up in Governance contract

--- contracts/misc/access/Governance.sol
+++ contracts/misc/access/Governance.sol
@@ -78,7 +78,13 @@ contract Governance is ControllableByContract {
             revert Unauthorized();
         }
         (bool success, bytes memory returnData) = target.call{gas: gasleft(), value: msg.value}(data);
-        require(success, string(returnData));
+
+        if (!success) {
+            assembly {
+                revert(add(returnData, 32), returnData)
+            }
+        }
+
         return returnData;
     }
 }
