aaf7e6102b1583fbb1b6a9a5b231ac14ce83fb02
donosonaumczuk <adonoso@itba.edu.ar>
2023-06-05 19:00:52 +0100

    misc: TODO added regarding ProtocolState enum order

--- contracts/libraries/constants/Types.sol
+++ contracts/libraries/constants/Types.sol
@@ -46,6 +46,8 @@ library Types {
      * @param Paused The fully paused state.
      */
     enum ProtocolState {
+        // TODO: Reverse order, so Paused becomes 0 and the default. This will get rid of the initializer needs.
+        // It needs to be done carefully, as it might pause the protocol in the Lens V2 upgrade.
         Unpaused,
         PublishingPaused,
         Paused
