fe137721a103f30a0b7e4e4767ddc49dde1284ab
donosonaumczuk <adonoso@itba.edu.ar>
2023-10-27 16:24:39 -0300

    misc: Token URI (temporarily) completely replaced

--- contracts/libraries/token-uris/FollowTokenURILib.sol
+++ contracts/libraries/token-uris/FollowTokenURILib.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.15;
 
 import {Base64} from '@openzeppelin/contracts/utils/Base64.sol';
 import {Strings} from '@openzeppelin/contracts/utils/Strings.sol';
-import {TokenURIMainFontLib} from 'contracts/libraries/token-uris/TokenURIMainFontLib.sol';
-import {TokenURISecondaryFontLib} from 'contracts/libraries/token-uris/TokenURISecondaryFontLib.sol';
+import {ImageTokenURILib} from 'contracts/libraries/token-uris/ImageTokenURILib.sol';
 
 library FollowTokenURILib {
     using Strings for uint96;
@@ -31,7 +30,7 @@ library FollowTokenURILib {
                             ' of Profile #',
                             followedProfileIdAsString,
                             '","image":"data:image/svg+xml;base64,',
-                            _getSVGImageBase64Encoded(followTokenIdAsString, followedProfileIdAsString),
+                            ImageTokenURILib.getSVGImageBase64Encoded(),
                             '","attributes":[{"display_type": "number", "trait_type":"ID","value":"',
                             followTokenIdAsString,
                             '"},{"trait_type":"DIGITS","value":"',
@@ -41,31 +40,6 @@ library FollowTokenURILib {
                             '"}]}'
                         )
                     )
-                )
-            );
-    }
-
-    function _getSVGImageBase64Encoded(string memory followTokenIdAsString, string memory followedProfileIdAsString)
-        private
-        pure
-        returns (string memory)
-    {
-        return
-            Base64.encode(
-                abi.encodePacked(
-                    '<svg width="724" height="724" viewBox="0 0 724 724" fill="none" xmlns="http://www.w3.org/2000/svg"><defs><style>',
-                    TokenURIMainFontLib.getFontBase64Encoded(),
-                    '</style></defs><defs><style>',
-                    TokenURISecondaryFontLib.getFontBase64Encoded(),
-                    '</style></defs><g clip-path="url(#clip0_2600_6938)"><rect width="724" height="724" fill="#D0DBFF"/><rect x="91" y="290" width="543" height="144" rx="72" fill="#FFEBB8"/><text fill="#5A4E4C" font-family="',
-                    TokenURIMainFontLib.getFontName(),
-                    '" font-size="42" letter-spacing="-1.5px"><tspan x="278" y="393.182">#',
-                    followTokenIdAsString,
-                    '</tspan></text><text fill="#5A4E4C" fill-opacity="0.7" font-family="',
-                    TokenURISecondaryFontLib.getFontName(),
-                    '" font-size="26" letter-spacing="-0.2px"><tspan x="280" y="347.516">Following #',
-                    followedProfileIdAsString,
-                    '</tspan></text><path d="M215.667 344.257C215.188 344.745 214.736 345.238 214.275 345.726C214.275 345.051 214.316 344.358 214.316 343.692C214.316 343.026 214.316 342.291 214.284 341.598C213.498 312.801 170.5 312.801 169.714 341.598C169.696 342.291 169.687 342.989 169.687 343.692C169.687 344.381 169.709 345.06 169.727 345.726C169.275 345.238 168.823 344.745 168.335 344.257C167.847 343.769 167.341 343.272 166.844 342.798C146.095 322.996 115.712 353.709 135.326 374.606C135.802 375.11 136.285 375.612 136.776 376.111C160.444 400 191.999 400 191.999 400C191.999 400 223.558 400 247.226 376.111C247.72 375.615 248.203 375.113 248.676 374.606C268.291 353.686 237.889 322.996 217.158 342.798C216.657 343.272 216.146 343.76 215.667 344.257Z" fill="#5A4E4C" fill-opacity="0.25"/><path d="M210.278 365.9C209.551 365.9 208.843 365.955 208.16 366.059C209.851 366.926 211.01 368.698 211.01 370.744C211.01 373.644 208.682 375.994 205.809 375.994C202.936 375.994 200.607 373.644 200.607 370.744C200.607 370.57 200.615 370.397 200.632 370.227C199.34 371.836 198.602 373.763 198.602 375.773H194.984C194.984 368.089 202.084 362.282 210.278 362.282C218.473 362.282 225.573 368.089 225.573 375.773H221.955C221.955 370.557 216.98 365.9 210.278 365.9ZM164.129 370.069C162.678 371.738 161.841 373.784 161.841 375.925H158.223C158.223 368.236 165.324 362.434 173.518 362.434C181.712 362.434 188.813 368.236 188.813 375.925H185.195C185.195 370.705 180.22 366.052 173.518 366.052C172.937 366.052 172.369 366.087 171.816 366.154C173.411 367.051 174.49 368.77 174.49 370.744C174.49 373.644 172.161 375.994 169.288 375.994C166.415 375.994 164.086 373.644 164.086 370.744C164.086 370.515 164.101 370.29 164.129 370.069ZM198.866 381.969C197.72 384.073 195.135 385.701 191.985 385.701C188.829 385.701 186.251 384.09 185.107 381.974L181.924 383.694C183.764 387.098 187.64 389.319 191.985 389.319C196.338 389.319 200.207 387.07 202.043 383.7L198.866 381.969Z" fill="#5A4E4C"/></g><defs><clipPath id="clip0_2600_6938"><rect width="724" height="724" fill="white"/></clipPath></defs></svg>'
                 )
             );
     }
--- contracts/libraries/token-uris/ProfileTokenURILib.sol
+++ contracts/libraries/token-uris/ProfileTokenURILib.sol
@@ -4,8 +4,7 @@ pragma solidity ^0.8.15;
 
 import {Base64} from '@openzeppelin/contracts/utils/Base64.sol';
 import {Strings} from '@openzeppelin/contracts/utils/Strings.sol';
-import {TokenURIMainFontLib} from 'contracts/libraries/token-uris/TokenURIMainFontLib.sol';
-import {TokenURISecondaryFontLib} from 'contracts/libraries/token-uris/TokenURISecondaryFontLib.sol';
+import {ImageTokenURILib} from 'contracts/libraries/token-uris/ImageTokenURILib.sol';
 import {StorageLib} from 'contracts/libraries/StorageLib.sol';
 
 library ProfileTokenURILib {
@@ -25,7 +24,7 @@ library ProfileTokenURILib {
                             '","description":"Lens Protocol - Profile #',
                             profileIdAsString,
                             '","image":"data:image/svg+xml;base64,',
-                            _getSVGImageBase64Encoded(profileIdAsString),
+                            ImageTokenURILib.getSVGImageBase64Encoded(),
                             '","attributes":[{"display_type": "number", "trait_type":"ID","value":"',
                             profileIdAsString,
                             '"},{"trait_type":"HEX ID","value":"',
@@ -37,25 +36,6 @@ library ProfileTokenURILib {
                             '"}]}'
                         )
                     )
-                )
-            );
-    }
-
-    function _getSVGImageBase64Encoded(string memory profileIdAsString) private pure returns (string memory) {
-        return
-            Base64.encode(
-                abi.encodePacked(
-                    '<svg width="724" height="724" viewBox="0 0 724 724" fill="none" xmlns="http://www.w3.org/2000/svg"><defs><style>',
-                    TokenURIMainFontLib.getFontBase64Encoded(),
-                    '</style></defs><defs><style>',
-                    TokenURISecondaryFontLib.getFontBase64Encoded(),
-                    '</style></defs><g clip-path="url(#clip0_2578_6938)"><rect width="724" height="724" fill="#C3E4CD"/><rect x="164" y="165" width="396" height="396" rx="20.5645" fill="#FFEBB8"/><text opacity="0.7" fill="#5A4E4C" font-family="',
-                    TokenURISecondaryFontLib.getFontName(),
-                    '" text-anchor="middle" font-size="26" letter-spacing="-0.2px"><tspan x="50%" y="504.748">Lens Profile</tspan></text><text fill="#5A4E4C" text-anchor="middle" font-family="',
-                    TokenURIMainFontLib.getFontName(),
-                    '" font-size="42" letter-spacing="-1.5px"><tspan x="50%" y="468.182">#',
-                    profileIdAsString,
-                    '</tspan></text><path d="M404.047 281.45C403.207 282.305 402.414 283.167 401.605 284.021C401.605 282.839 401.676 281.626 401.676 280.461C401.676 279.295 401.676 278.01 401.621 276.796C400.241 226.401 324.807 226.401 323.428 276.796C323.396 278.01 323.38 279.231 323.38 280.461C323.38 281.666 323.42 282.855 323.451 284.021C322.659 283.167 321.866 282.305 321.009 281.45C320.153 280.596 319.265 279.726 318.393 278.896C281.992 244.243 228.687 297.991 263.098 334.56C263.933 335.443 264.781 336.321 265.643 337.194C307.166 379 362.524 379 362.524 379C362.524 379 417.891 379 459.414 337.194C460.28 336.327 461.129 335.449 461.959 334.56C496.369 297.951 443.033 244.243 406.664 278.896C405.784 279.726 404.888 280.58 404.047 281.45Z" fill="black" fill-opacity="0.15"/><path d="M394.596 319.304C393.306 319.304 392.053 319.402 390.844 319.589C393.83 321.099 395.878 324.21 395.878 327.804C395.878 332.878 391.792 336.992 386.752 336.992C381.712 336.992 377.626 332.878 377.626 327.804C377.626 327.483 377.643 327.166 377.675 326.853C375.389 329.68 374.081 333.069 374.081 336.604H367.792C367.792 323.164 380.239 313.015 394.596 313.015C408.953 313.015 421.4 323.164 421.4 336.604H415.111C415.111 327.468 406.372 319.304 394.596 319.304ZM313.637 326.572C311.068 329.503 309.585 333.103 309.585 336.87H303.296C303.296 323.422 315.744 313.282 330.1 313.282C344.456 313.282 356.904 323.422 356.904 336.87H350.615C350.615 327.727 341.877 319.571 330.1 319.571C329.07 319.571 328.063 319.633 327.084 319.753C329.9 321.318 331.808 324.336 331.808 327.804C331.808 332.878 327.722 336.992 322.682 336.992C317.641 336.992 313.556 332.878 313.556 327.804C313.556 327.386 313.583 326.975 313.637 326.572ZM374.599 347.454C372.584 351.143 368.039 353.999 362.5 353.999C356.949 353.999 352.416 351.174 350.407 347.464L344.877 350.459C348.1 356.409 354.89 360.288 362.5 360.288C370.123 360.288 376.9 356.36 380.118 350.469L374.599 347.454Z" fill="#5A4E4C"/></g><defs><clipPath id="clip0_2578_6938"><rect width="724" height="724" fill="white"/></clipPath></defs></svg>'
                 )
             );
     }
